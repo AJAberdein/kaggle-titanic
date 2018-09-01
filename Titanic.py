1e8bb4d526e1e205b329a3ec958b12d26fdb63b9
AJAberdein <ashley@aberdein.co.za>
2018-09-01 18:09:44 +0200

    install dependencies and pull data

--- Titanic.py
+++ Titanic.py
@@ -1,234 +1,60 @@
 
 # coding: utf-8
 
-# In[200]:
+# In[283]:
 
+import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import re
+import xgboost as xgb
+import seaborn as sns
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
 
 
-# In[201]:
+# In[284]:
 
-train_data = pd.read_csv('data/train.csv')
-test_data = pd.read_csv('data/test.csv')
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
 
 
-# In[202]:
+# In[285]:
 
-train_data.head()
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from IPython.display import Image as PImage
+from subprocess import check_call
+from PIL import Image, ImageDraw, ImageFont
 
 
-# In[184]:
+# In[287]:
 
-test_data.head()
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
 
 
-# In[185]:
+# In[288]:
 
-train_data.info()
+PassengerId = test['PassengerId']
 
 
-# In[186]:
+# In[289]:
 
-train_data.describe()
+original_train = train.copy() #pass by reference
 
 
-# In[203]:
+# In[290]:
 
-tree = DecisionTreeClassifier(random_state=0)
+full_data = [train, test]
 
 
-# In[204]:
+# In[291]:
 
-train_data=train_data.replace({'Sex':{'male':0,'female':1}})
-test_data=test_data.replace({'Sex':{'male':0,'female':1}})
-
-
-# In[205]:
-
-train_data.drop('Name', axis=1, inplace=True)
-train_data.drop('Ticket', axis=1, inplace=True)
-
-
-# In[206]:
-
-test_data.drop('Name', axis=1, inplace=True)
-test_data.drop('Ticket', axis=1, inplace=True)
-
-
-# In[207]:
-
-train_data.head()
-
-
-# In[208]:
-
-train_data.drop('Embarked', axis=1, inplace=True)
-train_data.drop('Cabin', axis=1, inplace=True)
-
-
-# In[209]:
-
-test_data.drop('Embarked', axis=1, inplace=True)
-test_data.drop('Cabin', axis=1, inplace=True)
-
-
-# In[210]:
-
-train_data.head()
-
-
-# In[211]:
-
-test_data.head()
-
-
-# In[212]:
-
-X_train = train_data
-
-
-# In[ ]:
-
-
-
-
-# In[197]:
-
-X_train.drop('Survived', axis=1, inplace=True)
-
-
-# In[ ]:
-
-
-
-
-# In[213]:
-
-X_train.head()
-
-
-# In[214]:
-
-train_data
-
-
-# In[215]:
-
-y_train = train_data['Survived']
-
-
-# In[173]:
-
-y_train
-
-
-# In[218]:
-
-X_train.info()
-
-
-# In[220]:
-
-X_train.isnull().sum()
-
-
-# In[222]:
-
-age_value = X_train.Age.value_counts(normalize=True,dropna=True)
-
-
-# In[223]:
-
-age_value
-
-
-# In[228]:
-
-import numpy as np # linear algebra
-
-
-# In[231]:
-
-new_ages = np.random.choice(X_train.Age.dropna().values,size=len(X_train[X_train['Age'].isnull()]),replace=False)
-
-
-# In[232]:
-
-new_ages = pd.Series(data=new_ages,index=X_train[X_train.Age.isnull()].index)
-
-
-# In[233]:
-
-new_ages
-
-
-# In[234]:
-
-X_train['Age'].fillna(new_ages,inplace=True)
-
-
-# In[235]:
-
-X_train.info()
-
-
-# In[236]:
-
-X_train.isnull().sum()
-
-
-# In[237]:
-
-tree.fit(X_train, y_train)
-
-
-# In[259]:
-
-drop_elements = ['PassengerId', 'SibSp']
-test_data = test_data.drop(drop_elements, axis = 1)
-
-
-# In[260]:
-
-drop_elements = ['Age']
-test_data = test_data.drop(drop_elements, axis = 1)
-
-
-# In[261]:
-
-X_test = test_data
-
-
-# In[263]:
-
-X_test.info()
-
-
-# In[267]:
-
-X_test.isnull().sum()
-
-
-# In[268]:
-
-X_test.head()
-
-
-# In[275]:
-
-drop_elements = ['Fare']
-X_test = X_test.drop(drop_elements, axis = 1)
-
-
-# In[276]:
-
-X_test.isnull().sum()
-
-
-# In[277]:
-
-y_pred = tree.predict(X_test)
+train.head(3)
 
 
 # In[ ]:
