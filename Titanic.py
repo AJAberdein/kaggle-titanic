d380ec8e80be08bd45c9102bf61d6462f0432c02
AJAberdein <ashley@aberdein.co.za>
2018-09-01 14:18:58 +0200

    init

---
+++ Titanic.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+
+# In[6]:
+
+train_data = pd.read_csv('data/train.csv')
+test_data = pd.read_csv('data/test.csv')
+
+
+# In[4]:
+
+train_data.head()
+
+
+# In[7]:
+
+test_data.head()
+
+
+# In[ ]:
+
+
+
